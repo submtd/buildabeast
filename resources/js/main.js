1bf52a1e6c3682038c92c48419c8efe4be298258
Steve Harmeyer <harmeyersteve@gmail.com>
2022-03-08 18:48:38 -0700

    remove csrf verification for api requests

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,10 +3,7 @@ import "bootstrap";
 // Axios
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if(token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-}
+// Get Props
 let networkId = document.head.querySelector('meta[name="network-id"]');
 let networkName = document.head.querySelector('meta[name="network-name"]');
 // Vue
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="network-id" content="{{ $networkId }}">
         <meta name="network-name" content="{{ $networkName }}">
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
